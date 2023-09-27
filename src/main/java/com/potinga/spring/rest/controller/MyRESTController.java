1c66543c6a739186ce8c95a73cc851f07c333fc3
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-09-27 18:31:38 +0300

    Update MyRESTController.java

--- src/main/java/com/potinga/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/potinga/spring/rest/controller/MyRESTController.java
@@ -1,12 +1,13 @@
 package com.potinga.spring.rest.controller;
 
 import com.potinga.spring.rest.entity.Employee;
+import com.potinga.spring.rest.exception_handling.EmployeeIncorrectData;
+import com.potinga.spring.rest.exception_handling.NoSuchEmployeeException;
 import com.potinga.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,14 +16,38 @@ import java.util.List;
 public class MyRESTController {
     @Autowired
     private EmployeeService employeeService;
+
     @GetMapping("/employees")
-    public List<Employee> showAllEmployees(){
-        List<Employee>allEmployees = employeeService.getAllEmployees();
+    public List<Employee> showAllEmployees() {
+        List<Employee> allEmployees = employeeService.getAllEmployees();
         return allEmployees;
     }
+
     @GetMapping("/employees/{id}")
-    public Employee getEmployee(@PathVariable int id){
+    public Employee getEmployee(@PathVariable int id) {
         Employee employee = employeeService.getEmployee(id);
+        if (employee == null) {
+            throw new NoSuchEmployeeException("There is no employee with ID = " +
+                    id + " in Database");
+        }
         return employee;
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleExcepition(
+            NoSuchEmployeeException exception) {
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+
+        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleExcepition(
+            Exception exception) {
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+
+        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
     }
 }
