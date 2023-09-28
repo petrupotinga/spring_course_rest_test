bfdb6fc178c528b468109e42909ecf0ac557155e
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-09-28 14:53:05 +0300

    Create EmployeeGlobalExceptionHandler.java

---
+++ src/main/java/com/potinga/spring/rest/exception_handling/EmployeeGlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.potinga.spring.rest.exception_handling;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class EmployeeGlobalExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleExcepition(
+            NoSuchEmployeeException exception) {
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+
+        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleExcepition(
+            Exception exception) {
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+
+        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
+    }
+}
