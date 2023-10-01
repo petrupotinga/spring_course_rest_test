c1ca1a58b448b202156f13297d2a4dc403265b85
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-10-01 22:47:07 +0300

    Update MyRESTController.java

--- src/main/java/com/potinga/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/potinga/spring/rest/controller/MyRESTController.java
@@ -39,9 +39,21 @@ public class MyRESTController {
         employeeService.saveEmployee(employee);
         return employee;
     }
+
     @PutMapping("/employees")
-    public Employee upDateEmployee(@RequestBody Employee employee){
+    public Employee upDateEmployee(@RequestBody Employee employee) {
         employeeService.saveEmployee(employee);
         return employee;
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+        Employee employee = employeeService.getEmployee(id);
+        if (employee == null) {
+            throw new NoSuchEmployeeException("There is no employee with ID = " +
+                    id + " in Database");
+        }
+        employeeService.deleteEmployee(id);
+        return "Employee with ID = " + id + " was deleted ";
     }
 }
